d64334cf04e5b7c9b563c9a96c78910898738105
Kishore Cheryala <kishore.cheryala@avanade.com>
2025-06-19 18:00:04 +0100

    Validation

--- api/Program.cs
+++ api/Program.cs
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
 
 var newSummaries = new[]
 {
-    "Brrrrr", "Yikes", "Frozen", "Hot", "Ouch", "StopIt"
+    "Bracing", "Yikes", "Frozen", "Hot", "Ouch", "Scorching"
 };
 
 var summaries = new[]
